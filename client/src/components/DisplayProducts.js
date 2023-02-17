595d88914737a109e2445df5e6d2769267dbe9f7
Naaniam <92287236+Naaniam@users.noreply.github.com>
2023-02-16 20:55:11 -0800

    Update DisplayProducts.js

--- client/src/components/DisplayProducts.js
+++ client/src/components/DisplayProducts.js
@@ -36,7 +36,7 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 
 const DisplayProducts = () => {
     const [ProductsList, setData] = useState();
-    const ContractAddress = "0xE4b876ed393E19FbD18eC99118647BcbFE5300F3" //"0xFa56954976bA7d616945c09A7e360499e7038d98";
+    const ContractAddress = "0xaeF49dc0A2f76C1160D7B6fdb66e35F9E3537923" //"0xFa56954976bA7d616945c09A7e360499e7038d98";
 
     async function requestAccount() {
         await window.ethereum.request({ method: "eth_requestAccounts" });
